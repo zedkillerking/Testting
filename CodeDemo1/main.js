7330a07b454e9d8d7be7845a4610a0f980a898b0
DoTuanHung <hungdtgch190219@fpt.edu.vn>
2020-09-15 08:43:49 +0700

    testting

---
+++ CodeDemo1/main.js
@@ -0,0 +1,4 @@
+const myMath = require('./myModule');
+
+console.log(myMath.double(3));
+console.log(myMath.rectangleArea(5,9));
---
+++ CodeDemo1/myModule.js
@@ -0,0 +1,11 @@
+var mathlib ={
+    double : function(x){
+        return x*2;
+    },
+    
+
+    rectangleArea : function( width , height ){
+        return width*height;
+    }
+};
+module.exports = mathlib;
