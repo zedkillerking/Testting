64e37538f9f7c119e78cee5916c79c84c95526a5
DoTuanHung <hungdtgch190219@fpt.edu.vn>
2020-09-15 13:07:34 +0700

    sever\

--- sever.js
+++ sever.js
@@ -1,8 +1,47 @@
-var http = require('http'); //1. import Node.js core module
+var http = require('http'); // Import Node.js core module
 
-var server= http.createServer(function(req,res){ // 2 -creating server
-    res.end('hello world!')
+var server = http.createServer(function (req, res) {   //create web server
+    if (req.url == '/') { //check the URL of the current request
+        
+        // set response header
+        res.writeHead(200, { 'Content-Type': 'text/html' }); 
+        
+        // set response content    
+        res.write('<html><body><p>This is home Page.</p></body></html>');
+        res.end();
+    
+    }
+    else if (req.url == "/student") {
+        
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is student Page.</p></body></html>');
+        res.end();
+    
+    }
+    else if (req.url == "/admin") {
+        
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is admin Page.</p></body></html>');
+        res.end();
+    
+    }
+    else if (req.url == "/login"){
+        
+        
+         res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<body><h2>Admin Login</h2></body>');
+       
+       res.write('<input type="text" name="email" class="text-field" placeholder="Email" />'); 
+       res.write('<input type="password" name="password" class="text-field" placeholder="Password" />');
+       res.write('<input type="submit" name="login"  class="button" value="Log In" />');    
+        
+        
+    }
+    else
+        res.end('Invalid Request!');
+
 });
-server.listen(5000); // 3. listen for any incoming requests\
+
+server.listen(5000); //6 - listen for any incoming requests
 
 console.log('Node.js web server at port 5000 is running..')
